73b300215ed1985b83094f62bd62219399be1993
Enes Aydın <enes.aydin@sahibinden.com>
2020-03-08 21:58:49 +0300

    junit-ios-appium

---
+++ junit-ios-appium/src/test/java/com/base/BaseClass.java
@@ -0,0 +1,117 @@
+package com.base;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.remote.IOSMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.html5.Location;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.net.URL;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+public abstract class BaseClass
+{
+    private static final Logger logger = LoggerFactory.getLogger(BaseClass.class);
+
+    private static final int WEB_DRIVER_WAIT_TIMEOUT = 15;
+
+    public AppiumDriver driver;
+    public WebDriverWait webDriverWait;
+
+    @BeforeEach
+    public void init()
+    {
+        driver = createIosDriver();
+    }
+
+    @AfterEach
+    public void close()
+    {
+        if (Objects.nonNull(driver))
+        {
+            driver.closeApp();
+            driver.quit();
+            driver = null;
+        }
+    }
+
+    public AppiumDriver createIosDriver()
+    {
+        int counter = 0;
+
+        try
+        {
+            File appDir = new File("/opt/");
+
+            File app = new File(appDir.getCanonicalPath(), "");
+
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
+
+            capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
+            capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "13.2");
+            capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Iphone XR");
+            capabilities.setCapability("newCommandTimeout", "600");
+            capabilities.setCapability("waitForAppScript", "$.delay(5000); $.acceptAlert();");
+            capabilities.setCapability("waitForQuiescence", "false");
+            capabilities.setCapability("usePrebuiltWDA", "true");
+            capabilities.setCapability("udid", "00008020-001675360CD1002E");
+            capabilities.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+            capabilities.setCapability("nativeInstrumentsLib", "true");
+            capabilities.setCapability("useJSONSource", true);
+            capabilities.setCapability(IOSMobileCapabilityType.LOCATION_SERVICES_ENABLED, true);
+            capabilities.setCapability(IOSMobileCapabilityType.LOCATION_SERVICES_AUTHORIZED, true);
+            capabilities.setCapability(IOSMobileCapabilityType.AUTO_ACCEPT_ALERTS, "false");
+            capabilities.setCapability(IOSMobileCapabilityType.IOS_INSTALL_PAUSE, "8000");
+
+            URL url = null;
+
+            try
+            {
+
+                url = new URL("http://127.0.0.1:4723/wd/hub");
+                driver = new IOSDriver(url, capabilities);
+
+                webDriverWait = new WebDriverWait(driver, WEB_DRIVER_WAIT_TIMEOUT);
+                setIOSDriverTimeout();
+                setLocation();
+            }
+            catch (Exception e)
+            {
+                logger.error("Failed to create driver with URL: " + url);
+            }
+            counter++;
+        }
+        catch (Exception ex)
+        {
+        }
+
+        while ((driver == null) && (counter < 10)) ;
+
+        if (Objects.isNull(driver)) throw new NullPointerException("IOS Driver is null...");
+
+        return driver;
+    }
+
+    private void setIOSDriverTimeout()
+    {
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    public void setLocation()
+    {
+        Location location = new Location(40.9617514, 29.1099772, 41);
+        driver.setLocation(location);
+    }
+
+}
---
+++ junit-ios-appium/src/test/java/com/test/SampleIosTest.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import com.base.BaseClass;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SampleIosTest extends BaseClass
+{
+    @Test
+    @DisplayName("sample ios test")
+    public void sampleTest()
+    {
+
+    }
+}
